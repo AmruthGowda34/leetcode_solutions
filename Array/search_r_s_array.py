4182c714f22770052930830d78dc657a7b033ff8
Amruth Gowda <amruthgowda5002@gmail.com>
2026-02-21 19:17:58 +0530

    Add search_r_sarray.py

---
+++ Array/search_r_s_array.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def search(self, nums, target):
+        low=0
+        high=len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                return mid
+            if nums[mid]<=nums[high]:
+                if nums[mid]<=target<=nums[high]:
+                    low=mid+1
+                else:
+                    high=mid-1
+            else:
+                if nums[low]<=target<=nums[mid]:
+                    high=mid-1
+                else:
+                    low=mid+1
+        return -1
+s1=Solution()
+nums=[16,18,22,1,3,4,5,7,8,10,11,12,13]
+print(s1.search(nums,18))
