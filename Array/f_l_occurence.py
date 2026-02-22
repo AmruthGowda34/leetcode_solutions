5132b809b3049e31f185ca92122c7d15695b93f6
Amruth Gowda <amruthgowda5002@gmail.com>
2026-02-22 14:25:43 +0530

    update f_l_occurence.py

--- Array/f_l_occurence.py
+++ Array/f_l_occurence.py
@@ -27,3 +27,14 @@ s1=Solution()
 nums=[5,7,7,8,8,10]
 target=7
 print(s1.searchRange(nums,target))
+
+# n=[1,2,3,3,3,3,3,5,6,8,9,9,10]
+# tar=3
+# f_o=-1
+# l_o=-1
+# for i in range(len(n)):
+#     if n[i]==tar:
+#         if f_o==-1:
+#             f_o=i
+#         l_o=i
+# print(f_o,l_o)
